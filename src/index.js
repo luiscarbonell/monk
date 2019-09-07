a9b89687b711ab0fef799d957b30bb0bf908f4d5
luiscarbonell5683 <luiscarbonell5683@gmail.com>
2019-09-07 15:58:35 -0400

    Initial commit v4

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-const factorial = require("./factorial");
+// const factorial = require("./factorial");
+//
+// module.exports = { factorial }
 
-module.exports = { factorial }
+function factorial(n) {
+  if(n > 1) return n * factorial(--n);
+  else return 1;
+}
+
+// CommonJS & AMD
+if (typeof define !== 'undefined' && define.amd) define([], function () { return factorial; });
+
+// Node.js
+if (typeof module !== 'undefined' && module.exports) module.exports = factorial;
+
+// Browser
+if (typeof window === 'object') window['factorial'] = factorial;
